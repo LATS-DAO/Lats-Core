3ca42a200686a6fac7ce441766b4e38ac5058e0e
SpectreSecurityCoin <development@SpectreSecurity.io>
2023-03-14 17:46:43 -0400

    Misc Fixes

--- src/budget/budgetmanager.cpp
+++ src/budget/budgetmanager.cpp
@@ -852,8 +852,11 @@ std::string CBudgetManager::GetRequiredPaymentsString(int nBlockHeight)
 
 CAmount CBudgetManager::GetTotalBudget(int nHeight)
 {
-    // 100% of block reward after V5.5 upgrade
-    CAmount nSubsidy = 1;
+	// 100% of block reward 
+    CAmount nSubsidy = GetBlockValue(nHeight);
+	
+    // 10% Block Reward
+    nSubsidy /= 10;
 
 
     // multiplied by the number of blocks in a cycle (144 on testnet, 30*1440 on mainnet)
